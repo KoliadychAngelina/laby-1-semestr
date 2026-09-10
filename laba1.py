e6aed3214012fcdb4426572370d2b8ef8cba3395
KoliadychAngelina <k2jrckhc2s@privaterelay.appleid.com>
2026-09-10 21:11:39 +0300

    Add files via upload
    Lab1

---
+++ laba1.py
@@ -0,0 +1,25 @@
+print('Hello, World')
+
+u = 5
+j = 2.67
+d = 'Python'
+b = True
+k = False
+
+print (u, type (u))
+print (j, type (j))
+print (d, type (d))
+print (b, type (b))
+print (k, type (k))
+
+
+o = 1
+t = 67
+
+print (t+o)
+print (t-o)
+print (t*o)
+print (t/o)
+print (t**o)
+print (t//o)
+print (t%o)
