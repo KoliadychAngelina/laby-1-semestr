7957f10bce52be814e2cf0e14cbf21a90a416377
KoliadychAngelina <k2jrckhc2s@privaterelay.appleid.com>
2026-09-16 16:00:03 +0300

    Enhance laba1.py with new data structures
    Added new data structures and print statements for debugging.

--- laba1.py
+++ laba1.py
@@ -1,16 +1,36 @@
-print('Hello, World')
+print('Hello, World!')
 
 u = 5
 j = 2.67
 d = 'Python'
 b = True
 k = False
+my_list = [1, 3, 5, 7] #list
+my_turple = (2,4,6) #tuple
+my_dict = {9: 6,7: 8} #dict
+my_set = {1,2,3} #set
+
+
+print('------------------------')
+
+print('Вік собаки ', u)
+print('Довжина ', j)
+print('Мова програмування - ', d)
+print('Зараз вересень - ', b)
+
+print('------------------------')
 
 print (u, type (u))
 print (j, type (j))
 print (d, type (d))
 print (b, type (b))
 print (k, type (k))
+print (my_list , type (my_list))
+print (my_turple , type (my_turple))
+print (my_dict, type (my_dict))
+print (my_set, type (my_set))
+
+print('------------------------')
 
 
 o = 1
@@ -23,3 +43,8 @@ print (t/o)
 print (t**o)
 print (t//o)
 print (t%o)
+
+
+
+login = input("Введіть пароль та логін ")
+print(login)
